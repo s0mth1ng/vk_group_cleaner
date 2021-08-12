1167387897a81cf68c3738622a8e3723bb0f774b
Maxim Ivanov <maximsmthivanov@gmail.com>
2021-08-12 03:42:53 +0300

    added to csv option

--- group_cleaner.py
+++ group_cleaner.py
@@ -27,6 +27,13 @@ def get_confirmation(groups) -> bool:
     return True if input('Are you sure? (y/n) ').lower() == 'y' else False
 
 
+def save_to_csv(groups, filename='unsubscribed.csv', sep=','):
+    with open(filename, 'w') as f:
+        f.write(f'id{sep}name{sep}url\n')
+        for g in groups:
+            f.write(f'{g.id}{sep}{g.name}{sep}{g.url}\n')
+
+
 def main(args):
     api = VkApi()
     groups = api.get_groups()
@@ -35,14 +42,19 @@ def main(args):
         print('Aborting...')
         exit(0)
     error_counter = 0
+    leaved = []
     for g in selected:
         try:
             api.leave_group(g.id)
+            leaved.append(g)
             print(f'Successfully left "{g.name}" :)')
         except ApiException as e:
             error_counter += 1
             print(f'Cannot leave "{g.name} :("')
             print(f'Error message: {str(e)}')
+    if args.to_csv:
+        print('Saving to unsubscribed.csv...')
+        save_to_csv(leaved)
     print(f'Total errors: {error_counter}. Bye')
 
 
@@ -50,4 +62,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--at_once", help="print that number of subscriptions at once", type=int, default=10)
+    parser.add_argument(
+        "--to_csv", help="save unsubscribed groups to csv file", default=False, action='store_true')
     main(parser.parse_args())
